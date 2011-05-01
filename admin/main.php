4b16fa6fb0ea3de055f72018cc19a59c523735a2
designburo <designburo@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-05-01 08:58:54 +0000

    Added QRCode wizard generator. GUI Only. No functionality yet.

--- admin/main.php
+++ admin/main.php
@@ -18,26 +18,26 @@ if(isset($_GET['a']))
 	{
 		
 	case "url":
-		echo '<table width="400"><tr><td width="800"><div style="margin:30px;padding:10px;border:1px solid #aaa;width:800px;"><div style="margin-bottom:5px;border-bottom:1px solid #aaa;"><h3>'._QRCODE_WIZARD_H.' :: '._QRCODE_URL_H.'</h3><font size="-2">'._QRCODE_WIZARD_D.'</font><BR><BR></div><div style="padding:10px;">';
+		echo '<table width="400"><tr><td width="800"><div style="margin:30px;padding:10px;border:1px solid #aaa;width:700px;"><div style="margin-bottom:5px;border-bottom:1px solid #aaa;"><h3>'._QRCODE_WIZARD_H.' :: '._QRCODE_URL_H.'</h3><font size="-2">'._QRCODE_WIZARD_D.'</font><BR><BR></div><div style="padding:10px;">';
 		echo '<table><tr><td width="150" align="left"><img src="../images/create_url.png" border="0" title="'._QRCODE_URL_D.'"></td><td>';
 		echo '<h2>'._QRCODE_URL_D.'</h2>'._QRCODE_FORM.'<BR><BR>';
 		echo '<form id="form1" name="form1" method="post" action="main.php">';
-		echo '<div id="url_div"><table class="tabel"><tr><td>URL</td><td><input name="qr_url" type="text" size="50" /></td><td align=right><input name="submit" id="submit" type="submit" value="OK" /></td>
+		echo '<div id="url_div"><table class="tabel"><tr><td>'._QRCODE_URL_F_URL.'</td><td><input name="qr_url" type="text" size="50" /></td><td align=right><input name="submit" id="submit" type="submit" value="'._QRCODE_FORM_SUBMIT.'" /></td>
     </tr></table></td></tr></table>
     </div></form></div></div></td></tr></table>';
 	break;
 	
 	case "email":
-		echo '<table width="400"><tr><td width="800"><div style="margin:30px;padding:10px;border:1px solid #aaa;width:800px;"><div style="margin-bottom:5px;border-bottom:1px solid #aaa;"><h3>'._QRCODE_WIZARD_H.' :: '._QRCODE_EMAIL_H.'</h3><font size="-2">'._QRCODE_WIZARD_D.'</font><BR><BR></div><div style="padding:10px;">';
+		echo '<table width="400"><tr><td width="800"><div style="margin:30px;padding:10px;border:1px solid #aaa;width:700px;"><div style="margin-bottom:5px;border-bottom:1px solid #aaa;"><h3>'._QRCODE_WIZARD_H.' :: '._QRCODE_EMAIL_H.'</h3><font size="-2">'._QRCODE_WIZARD_D.'</font><BR><BR></div><div style="padding:10px;">';
 		echo '<table><tr><td width="150" align="left"><img src="../images/create_email.png" border="0" title="'._QRCODE_EMAIL_D.'"></td><td>';
 		echo '<h2>'._QRCODE_EMAIL_D.'</h2>'._QRCODE_FORM.'<BR><BR>';
 		echo '<form id="form1" name="form1" method="post" action="main.php">';
-		echo '<div id="url_div"><table class="tabel"><tr><td>Email to</td><td><input name="qr_email_to" type="text" size="50" /></td><td rowspan="3"align="right" valign="bottom"><input name="submit" id="submit" type="submit" value="OK" /></td>
+		echo '<div id="url_div"><table class="tabel"><tr><td>'._QRCODE_EMAIL_F_TO.'</td><td><input name="qr_email_to" type="text" size="50" /></td><td rowspan="3"align="right" valign="bottom"><input name="submit" id="submit" type="submit" value="'._QRCODE_FORM_SUBMIT.'" /></td>
     </tr>';
 		echo '<tr>
-    <td>Email subject</td><td><input name="qr_email_sub" type="text" size="50" /></td>
+    <td valign="middle">'._QRCODE_EMAIL_F_SUBJECT.'</td><td valign="top"><input name="qr_email_sub" type="text" size="50" /></td>
     </tr><tr>    
-    <td>Email text</td><td><input name="qr_email_txt" type="text" size="50" /></td>
+    <td>'._QRCODE_EMAIL_F_TXT.'</td><td><input name="qr_email_txt" type="text" size="50" /></td>
     </tr>
 	
 	';
@@ -45,7 +45,100 @@ if(isset($_GET['a']))
     </div></form></div></div></td></tr></table>';
 	break;
 
+	case "phone":
+		echo '<table width="400"><tr><td width="800"><div style="margin:30px;padding:10px;border:1px solid #aaa;width:700px;"><div style="margin-bottom:5px;border-bottom:1px solid #aaa;"><h3>'._QRCODE_WIZARD_H.' :: '._QRCODE_PHONE_H.'</h3><font size="-2">'._QRCODE_WIZARD_D.'</font><BR><BR></div><div style="padding:10px;">';
+		echo '<table><tr><td width="150" align="left"><img src="../images/create_phone.png" border="0" title="'._QRCODE_PHONE_D.'"></td><td>';
+		echo '<h2>'._QRCODE_PHONE_D.'</h2>'._QRCODE_FORM.'<BR><BR>';
+		echo '<form id="form1" name="form1" method="post" action="main.php">';
+		echo '<div id="url_div"><table class="tabel"><tr><td>'._QRCODE_PHONE_F_NR.'</td><td><input name="qr_phone" type="text" size="50" /></td><td align=right><input name="submit" id="submit" type="submit" value="'._QRCODE_FORM_SUBMIT.'" /></td>
+    </tr></table></td></tr></table>
+    </div></form></div></div></td></tr></table>';
+	break;
+
+	case "sms":
+		echo '<table width="400"><tr><td width="800"><div style="margin:30px;padding:10px;border:1px solid #aaa;width:700px;"><div style="margin-bottom:5px;border-bottom:1px solid #aaa;"><h3>'._QRCODE_WIZARD_H.' :: '._QRCODE_SMS_H.'</h3><font size="-2">'._QRCODE_WIZARD_D.'</font><BR><BR></div><div style="padding:10px;">';
+		echo '<table><tr><td width="150" align="left"><img src="../images/create_sms.png" border="0" title="'._QRCODE_SMS_D.'"></td><td>';
+		echo '<h2>'._QRCODE_SMS_D.'</h2>'._QRCODE_FORM.'<BR><BR>';
+		echo '<form id="form1" name="form1" method="post" action="main.php">';
+		echo '<div id="url_div"><table class="tabel"><tr><td>'._QRCODE_SMS_F_NR.'</td><td><input name="qr_sms_to" type="text" size="50" maxsize="140" /></td><td rowspan="3"align="right" valign="bottom"><input name="submit" id="submit" type="submit" value="'._QRCODE_FORM_SUBMIT.'" /></td>
+    </tr>';
+		echo '<tr>
+    <td>'._QRCODE_SMS_F_TXT.'</td><td><input name="qr_sms_txt" type="text" size="50" /></td>
+    </tr>
 	
+	';
+	echo '</table></td></tr></table>
+    </div></form></div></div></td></tr></table>';
+	break;
+
+	case "txt":
+		echo '<table width="400"><tr><td width="800"><div style="margin:30px;padding:10px;border:1px solid #aaa;width:700px;"><div style="margin-bottom:5px;border-bottom:1px solid #aaa;"><h3>'._QRCODE_WIZARD_H.' :: '._QRCODE_TXT_H.'</h3><font size="-2">'._QRCODE_WIZARD_D.'</font><BR><BR></div><div style="padding:10px;">';
+		echo '<table><tr><td width="150" align="left"><img src="../images/create_txt.png" border="0" title="'._QRCODE_TXT_D.'"></td><td>';
+		echo '<h2>'._QRCODE_TXT_D.'</h2>'._QRCODE_FORM.'<BR><BR>';
+		echo '<form id="form1" name="form1" method="post" action="main.php">';
+		echo '<div id="url_div"><table class="tabel"><tr><td>'._QRCODE_TXT_F_TXT.'</td><td><textarea "qr_txt" cols="45" rows="3" /></textarea></td><td rowspan="3"align="right" valign="bottom"><input name="submit" id="submit" type="submit" value="'._QRCODE_FORM_SUBMIT.'" /></td>
+    </tr>';
+
+	echo '</table></td></tr></table>
+    </div></form></div></div></td></tr></table>';
+	break;
+	
+	case "geo":
+		echo '<table width="400"><tr><td width="800"><div style="margin:30px;padding:10px;border:1px solid #aaa;width:700px;"><div style="margin-bottom:5px;border-bottom:1px solid #aaa;"><h3>'._QRCODE_WIZARD_H.' :: '._QRCODE_GEO_H.'</h3><font size="-2">'._QRCODE_WIZARD_D.'</font><BR><BR></div><div style="padding:10px;">';
+		echo '<table><tr><td width="150" align="left"><img src="../images/create_geo.png" border="0" title="'._QRCODE_GEO_D.'"></td><td>';
+		echo '<h2>'._QRCODE_GEO_D.'</h2>'._QRCODE_FORM.'<BR><BR>';
+		echo '<form id="form1" name="form1" method="post" action="main.php">';
+		echo '<div id="url_div"><table class="tabel"><tr><td>'._QRCODE_GEO_F_LA.'</td><td><input name="qr_latitude" type="text" size="50" /></td><td rowspan="3"align="right" valign="bottom"><input name="submit" id="submit" type="submit" value="'._QRCODE_FORM_SUBMIT.'" /></td>
+    </tr>';
+		echo '<tr>
+    <td>'._QRCODE_GEO_F_LO.'</td><td><input name="qr_longtitude" type="text" size="50" /></td>
+    </tr>
+	
+	';
+	echo '</table></td></tr></table>
+    </div></form></div></div></td></tr></table>';
+	break;	
+	
+	
+	case "bus":
+		echo '<table width="400"><tr><td width="800"><div style="margin:30px;padding:10px;border:1px solid #aaa;width:700px;"><div style="margin-bottom:5px;border-bottom:1px solid #aaa;"><h3>'._QRCODE_WIZARD_H.' :: '._QRCODE_BUS_H.'</h3><font size="-2">'._QRCODE_WIZARD_D.'</font><BR><BR></div><div style="padding:10px;">';
+		echo '<table><tr><td width="150" align="left"><img src="../images/create_contact.png" border="0" title="'._QRCODE_BUS_D.'"></td><td>';
+		echo '<h2>'._QRCODE_BUS_D.'</h2>'._QRCODE_FORM.'<BR><BR>';
+		echo '<form id="form1" name="form1" method="post" action="main.php">';
+		echo '<div id="url_div"><table class="tabel"><tr><td>'._QRCODE_BUS_F_SNAME.'</td><td><input name="qr_c_anaam" type="text" size="50" /></td><td rowspan="17"align="right" valign="bottom"><input name="submit" id="submit" type="submit" value="'._QRCODE_FORM_SUBMIT.'" /></td>
+    </tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_FNAME.'</td><td><input name="qr_c_vnaam" type="text" size="50" /></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_COMPANY.'</td><td><input name="qr_c_bedrijf" type="text" size="50" /></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_TITLE.'</td><td><input name="qr_c_functie" type="text" size="50" /></td></tr>';
+		echo '<tr><td colspan=2><BR><BR></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_PHONE.'</td><td><input name="qr_c_phone" type="text" size="50" /></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_MOB.'</td><td><input name="qr_c_gsm" type="text" size="50" /></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_EMAIL.'</td><td><input name="qr_c_email" type="text" size="50" /></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_WEB.'</td><td><input name="qr_c_web" type="text" size="50" /></td></tr>';
+		echo '<tr><td colspan=2><BR><BR></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_ADRES.'</td><td><input name="qr_c_adres" type="text" size="50" /></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_ZIP.'</td><td><input name="qr_c_zip" type="text" size="50" /></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_CITY.'</td><td><input name="qr_c_city" type="text" size="50" /></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_STATE.'</td><td><input name="qr_c_state" type="text" size="50" /></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_COUNTRY.'</td><td><input name="qr_c_land" type="text" size="50" /></td></tr>';
+		echo '<tr><td colspan=2><BR><BR></td></tr>';
+		echo '<tr><td>'._QRCODE_BUS_F_BDAY.'</td><td><select name="qr_c_gdag">';
+		for($rt=1;$rt<=31;$rt++){
+			echo "<option value=\"".sprintf('%02d',$rt)."\">".sprintf('%02d',$rt)."</option>\n";
+		}
+		echo '</select> / <select  name="qr_c_gmaand">';
+		for($rt=1;$rt<=12;$rt++){
+			echo "<option value=\"".sprintf('%02d',$rt)."\">".sprintf('%02d',$rt)."</option>\n";
+		}
+		echo '</select> / <select name="qr_c_gjaar">';
+		for($rt=2011;$rt>=1920;$rt--){
+			echo "<option value=\"".$rt."\">".$rt."</option>\n";
+		}
+		echo '</select></td></tr>';
+	echo '</table></td></tr></table>
+    </div></form></div></div></td></tr></table>';
+	break;	
+
 	}
 	
 	
@@ -53,145 +146,7 @@ if(isset($_GET['a']))
 	
 }
 else {
-?>
 
-<!--
-  <table class="tabel">
-  <tr><td><h2>QRCode generator</h2>
-  [nl]Gebruik de QRCode generator voor als je alleen een QRCode wilt aanmaken.[/nl][en]Use the QRCode generator if you only want to create a QRCode.[/en]</td></tr></table>
-<form id="form1" name="form1" method="post" action="">
-  <table class="tabel">
-    <tr>
-      <td><label>
-        <input type="radio" name="type" value="url" id="type_url" onclick="check(this);" />
-        [en]Webpage URL[/en][nl]Webpagina URL[/nl]</label></td>
-
-      <td><label>
-        <input type="radio" name="type" value="phone" id="type_phone" onclick="check(this);" />
-        [en]Phonenumber[/en][nl]Telefoonnummer[/nl]</label></td>
-
-      <td><label>
-        <input type="radio" name="type" value="sms" id="type_sms" onclick="check(this);" />
-        SMS</label></td>
-
-      <td><label>
-        <input name="type" type="radio" id="type_email" value="email" onclick="check(this);" />
-      Email</label></td>
-    </tr><tr>
-      <td><label>
-        <input type="radio" name="type" value="gps" id="type_gps" onclick="check(this);" />
-        [nl]GPS locatie[/nl][en]GPS Location[/en]</label></td>
-      <td><label>
-        <input type="radio" name="type" value="contact" id="type_contact" onclick="check(this);" />
-        [en]Business Card[/en][nl]Visitekaartje[/nl]</label></td>        
-      <td><label>
-        <input type="radio" name="type" value="txt" id="type_txt" onclick="check(this);" />
-        Text</label></td>
-    
-    </tr>
-  </table>
-  <div id="email_div">
-  <table class="tabel">
-    <tr>
-    <td>[en]Email to[/en][nl]E-mail aan[/nl]</td><td><input name="qr_email_to" type="text" size="50" /></td>
-    <td rowspan=3 align=right valign="bottom"><input name="submit" id="submit" type="submit" value="OK" /></td>
-    </tr><tr>
-    <td>[en]Email subject[/en][nl]Onderwerp[/nl]</td><td><input name="qr_email_sub" type="text" size="50" /></td>
-    </tr><tr>    
-    <td>[en]Email text[/en][nl]Bericht[/nl]</td><td><input name="qr_email_txt" type="text" size="50" /></td>
-    </tr>
-</table>
-    </div>
-  <div id="url_div">
-  <table class="tabel">
-    <tr>
-    <td>URL</td><td><input name="qr_url" type="text" size="50" /></td>
-    <td align=right><input name="submit" id="submit" type="submit" value="OK" /></td>
-    </tr></table>
-    </div> 
-  <div id="phone_div">
-  <table class="tabel">
-    <tr>
-    <td>[en]Phonenumber[/en][nl]Telefoonnummer[/nl]</td><td><input name="qr_phone" type="text" size="50" /></td>
-    <td align=right><input name="submit" id="submit" type="submit" value="OK" /></td>
-
-    
-    </tr></table>
-    </div>  
-  <div id="sms_div">
-  <table class="tabel">
-    <tr>
-    <td>[en]Phonenumber[/en][nl]Telefoonnummer[/nl]</td><td><input name="qr_sms_to" type="text" size="50" /></td><td rowspan=2 valign=bottom align=right><input name="submit" id="submit" type="submit" value="OK" /></td></tr>
-    <tr><td>Text</td><td><input name="qr_sms_txt" type="text" size="50" maxsize="140" /></td>
-    </tr></table>
-    </div>  
-  <div id="txt_div">
-  <table class="tabel">
-    <tr>
-    <td>Text</td><td><textarea name="qr_txt" cols="45" rows="3"></textarea></td>
-    <td align=right><input name="submit" id="submit" type="submit" value="OK" /></td>
-    </tr></table>
-    </div>
-  <div id="gps_div">
-  <table class="tabel">
-    <tr>
-    <td>latitude</td><td><input name="qr_latitude" type="text" size="50" /></td>
-    <td rowspan=2 align=right valign="bottom"><input name="submit" id="submit" type="submit" value="OK" /></td>
-    </tr><tr>
-    <td>longtitude</td><td><input name="qr_longtitude" type="text" size="50" /></td>
-    </tr></table>
-    </div>   
-  <div id="contact_div">
-  <table class="tabel">
-    <tr>
-    <td>[en]Sirname[/en][nl]Achternaam[/nl]</td><td><input name="qr_c_anaam" type="text" size="50" /></td>
-    <td rowspan=17 align=right valign="bottom"><input name="submit" id="submit" type="submit" value="OK" /></td>
-    </tr><tr>
-    <td>[en]Name[/en][nl]Voornaam[/nl]<td><input name="qr_c_vnaam" type="text" size="50" /></td>
-    </tr><tr>    
-    <td>[en]Company[/en][nl]Bedrijf[/nl]</td><td><input name="qr_c_bedrijf" type="text" size="50" /></td>
-    </tr>
-	<tr>    
-    <td>[en]Title[/en][nl]Functie[/nl]</td><td><input name="qr_c_functie" type="text" size="50" /></td>
-    </tr><tr><td colspan=2><HR /></td></tr>
-    <tr><td>[en]Phone[/en][nl]Telefoon[/nl]</td><td><input name="qr_c_phone" type="text" size="25" /></td></tr>
-    <tr><td>[en]Mobile[/en][nl]Mobiel[/nl]</td><td><input name="qr_c_gsm" type="text" size="25" /></td></tr>
-    <tr><td>[en]Email[/en][nl]E-mail[/nl]</td><td><input name="qr_c_email" type="text" size="50" /></td>
-    </tr>
-	<tr>    
-    <td>Website</td><td><input name="qr_c_web" type="text" size="50" /></td>
-    </tr>    
-    <tr><td colspan=2><HR /></td></tr>
-    <tr><td>[en]Adress[/en][nl]Adres[/nl]</td><td><input name="qr_c_adres" type="text" size="25" /></td></tr>
-    <tr><td>[en]Zipcode[/en][nl]Postcode[/nl]</td><td><input name="qr_c_zip" type="text" size="25" /></td></tr>
-    <tr><td>[en]City[/en][nl]Stad[/nl]</td><td><input name="qr_c_city" type="text" size="25" /></td></tr>
-    <tr><td>[en]State[/en][nl]Provincie[/nl]</td><td><input name="qr_c_state" type="text" size="25" /></td></tr>
-    <tr><td>[en]Country[/en][nl]Land[/nl]</td><td><input name="qr_c_land" type="text" size="25" /></td></tr>    
-	<tr><tr><td colspan=2><HR /></td></tr>
-	<tr>    
-    <td>[en]Birthday[/en][nl]Geboortedatum[/nl]</td><td><select name="qr_c_gdag"><?php
-	for($rt=1;$rt<=31;$rt++){
-		echo "<option value=\"".sprintf('%02d',$rt)."\">".sprintf('%02d',$rt)."</option>\n";
-	}?>
-
-</select> / <select  name="qr_c_gmaand"><?php
-	for($rt=1;$rt<=12;$rt++){
-		echo "<option value=\"".sprintf('%02d',$rt)."\">".sprintf('%02d',$rt)."</option>\n";
-	}?>
-
-</select> / <select name="qr_c_gjaar"><?php
-	for($rt=2005;$rt>=1920;$rt--){
-		echo "<option value=\"".$rt."\">".$rt."</option>\n";
-	}?>
-    </select>
-    </td>
-    </tr>     
- </table>
-    </div>
-</form>
--->
-
-<?php
 
 
 
--- language/english/main.php
+++ language/english/main.php
@@ -1,4 +1,5 @@
 <?php
+define('_QRCODE_FORM_SUBMIT','OK');
 define('_QRCODE_URL_H','Url');
 define('_QRCODE_TXT_H','Text');
 define('_QRCODE_SMS_H','Short Message Service (SMS)');
@@ -9,10 +10,36 @@ define('_QRCODE_EMAIL_H','Email');
 define('_QRCODE_URL_D','Visit a webpage url');
 define('_QRCODE_TXT_D','Create plain text');
 define('_QRCODE_SMS_D','Send a SMS message');
-define('_QRCODE_PHONE_D','Call a phonenumer');
+define('_QRCODE_PHONE_D','Call a phonenumber');
 define('_QRCODE_GEO_D','Visite geographical coordinates');
 define('_QRCODE_BUS_D','Create a Business card');
 define('_QRCODE_EMAIL_D','Send an email');
+
+define('_QRCODE_URL_F_URL','Url');
+define('_QRCODE_TXT_F_TXT','Text');
+define('_QRCODE_SMS_F_NR','Phonenumber');
+define('_QRCODE_SMS_F_TXT','Text');
+define('_QRCODE_PHONE_F_NR','Phonenumber');
+define('_QRCODE_GEO_F_LA','Latitude');
+define('_QRCODE_GEO_F_LO','Longtitide');
+define('_QRCODE_BUS_F_SNAME','Sirname');
+define('_QRCODE_BUS_F_FNAME','First name');
+define('_QRCODE_BUS_F_COMPANY','Company');
+define('_QRCODE_BUS_F_TITLE','Title');
+define('_QRCODE_BUS_F_PHONE','Phonenumber');
+define('_QRCODE_BUS_F_MOB','Mobile number');
+define('_QRCODE_BUS_F_EMAIL','Emailaddress');
+define('_QRCODE_BUS_F_WEB','Website url');
+define('_QRCODE_BUS_F_ADRES','Adres (street + number)');
+define('_QRCODE_BUS_F_ZIP','Zipcode');
+define('_QRCODE_BUS_F_CITY','City');
+define('_QRCODE_BUS_F_STATE','State');
+define('_QRCODE_BUS_F_COUNTRY','Country');
+define('_QRCODE_BUS_F_BDAY','Birthday (D/M/Y)');
+define('_QRCODE_EMAIL_F_TO','Email to');
+define('_QRCODE_EMAIL_F_SUBJECT','Email subject');
+define('_QRCODE_EMAIL_F_TXT','Email text');
+
 define('_QRCODE_FORM','Please fill in appropiate field(s) in the form to create the qrcode.');
 define('_QRCODE_WIZARD_H','QRCode wizard');
 define('_QRCODE_WIZARD_D','This wizard guides you in creating the QRCode.');
